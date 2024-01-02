66c1837faa66297bded75c33107be3bffc5e0456
Rodrigo Barboni Filho <68304421+rodrigobarbonifilho@users.noreply.github.com>
2024-01-02 17:17:15 -0300

    arrumando erros

--- src/Scripts/main.py
+++ src/Scripts/main.py
@@ -1,9 +1,9 @@
 from os import listdir
 from json import dump
 
-dict = {}
+images_list = []
 for i_image_file, image_file in enumerate(listdir('./..//public')):
-    dict[f"{i_image_file:3}".replace(" ", "0")] = image_file
+    images_list.append(image_file)
 
 with open("./src/images.json", "w") as file:
-    dump(dict, file, ensure_ascii=True, indent=4)
+    dump({"images": images_list}, file, ensure_ascii=True, indent=4)
